e4a4be0f5a7a86040db958c6bb944c6b6168ae8a
Leo <cni586@aucklanduni.ac.nz>
2025-11-18 21:23:17 +1300

    Initial Commit

---
+++ Larder/LarderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LarderApp.swift
+//  Larder
+//
+//  Created by Marlin on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LarderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
